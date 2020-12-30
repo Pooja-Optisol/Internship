0b0668dcec60677abe07b997afe5db47d0f7dcb1
Pooja-Optisol <74913069+Pooja-Optisol@users.noreply.github.com>
2020-12-30 19:37:06 +0530

    Add files via upload

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,13 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "testdb",
-    dialect: "mysql",
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+    HOST: process.env.HOST,
+    USER: process.env.USER,
+    PASSWORD: process.env.PASSWORD,
+    DB: process.env.DB
 };
